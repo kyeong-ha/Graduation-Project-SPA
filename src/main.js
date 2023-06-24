5352656f6488167d5b641cc7792e34ad8c34b5bc
KyeongHa <rudgk030@hanyang.ac.kr>
2023-06-24 23:18:03 +0900

    2023-06-24

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// we can use interacting the client with the backend services through the Amplify
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports)
+
 const app = createApp(App)
 
 app.use(router)
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      find: './runtimeConfig', // Vite Config
+      replacement: './runtimeConfig.browser' // Vite Config
+      
+      // '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
