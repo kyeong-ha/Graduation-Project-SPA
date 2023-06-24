31e6ba23ec1fa612059791aaff98b0ff896a143d
KyeongHa <rudgk030@hanyang.ac.kr>
2023-06-25 00:13:45 +0900

    2023-06-24

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div id="app">
     <h1>MBTI App</h1>
     <input type="text" v-model="num" placeholder="answer numㅊㅇ" />
@@ -47,4 +47,17 @@
       }
     }
   }
+</script> -->
+
+<script setup>
+  import { Authenticator } from '@aws-amplify/ui-vue';
+  import '@aws-amplify/ui-vue/styles.css';
 </script>
+<template>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+</template>
