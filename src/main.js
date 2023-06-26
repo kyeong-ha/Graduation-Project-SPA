89a59f3e3363116cfef044b6437cb2b7a5ae882c
KyeongHa <rudgk030@hanyang.ac.kr>
2023-06-26 22:01:46 +0900

    2023-06-26

--- src/main.js
+++ src/main.js
@@ -7,10 +7,19 @@ import router from './router'
 // we can use interacting the client with the backend services through the Amplify
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
-Amplify.configure(awsExports)
+
+Amplify.configure(awsExports);
+// import {
+//   applyPolyfills,
+//   defineCustomElements,
+// } from '@aws-amplify/ui-components/loader';
+
+// applyPolyfills().then(() => {
+//     defineCustomElements(window);
+// });
 
 const app = createApp(App)
+app.config.isCustomElement = tag => tag.startsWith('amplify-');
 
 app.use(router)
-
 app.mount('#app')
